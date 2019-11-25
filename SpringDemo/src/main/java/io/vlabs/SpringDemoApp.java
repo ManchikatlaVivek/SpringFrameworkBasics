3f5dbdb460f7703eeeaa31a2d869bd7e539c79c7
Vivek Manchikatla <vivekmanchikatla1996@gmail.com>
2019-11-25 20:26:34 +0530

    Lifecycle Callbacks - Init and Destroy of Beans

--- SpringDemo/src/main/java/io/vlabs/SpringDemoApp.java
+++ SpringDemo/src/main/java/io/vlabs/SpringDemoApp.java
@@ -1,6 +1,6 @@
 package io.vlabs;
 
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringDemoApp {
@@ -9,8 +9,9 @@ public class SpringDemoApp {
 
 	public static void main(String[] args) {
 
-		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 		
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		context.registerShutdownHook();
 		TrianglePoints trianglePoints = (TrianglePoints)context.getBean("trianglePoints");
 		trianglePoints.draw();
 
--- SpringDemo/src/main/java/io/vlabs/TrianglePoints.java
+++ SpringDemo/src/main/java/io/vlabs/TrianglePoints.java
@@ -1,16 +1,12 @@
 package io.vlabs;
 
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.BeanNameAware;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 
-public class TrianglePoints implements ApplicationContextAware, BeanNameAware {
-	
+public class TrianglePoints implements InitializingBean, DisposableBean {
 	private Point pointA;
 	private Point pointB;
 	private Point pointC;
-	private ApplicationContext context;
 	
 	public TrianglePoints() {
 		
@@ -48,22 +44,20 @@ public class TrianglePoints implements ApplicationContextAware, BeanNameAware {
 	}
 	
 	public void draw() {
-		System.out.println("Points are ("+pointA.getX()+" , "+pointA.getY()+")");
-		System.out.println("Points are ("+pointB.getX()+" , "+pointB.getY()+")");
-		System.out.println("Points are ("+pointC.getX()+" , "+pointC.getY()+")");
-	}
-	
-
-	@Override
-	public void setApplicationContext(ApplicationContext context) throws BeansException {
-		// TODO Auto-generated method stub
-		this.context = context;
+		System.out.println("Point A: ("+pointA.getX()+" , "+pointA.getY()+")");
+		System.out.println("Point B: ("+pointB.getX()+" , "+pointB.getY()+")");
+		System.out.println("Point C: ("+pointC.getX()+" , "+pointC.getY()+")");
 	}
 
 	@Override
-	public void setBeanName(String name) {
-		System.out.println("Bean name is: "+name);
+	public void afterPropertiesSet() throws Exception {
+		System.out.println("Init the bean");
 		
 	}
 
+	@Override
+	public void destroy() throws Exception {
+		System.out.println("The Beans are destroyed");
+		
+	}
 }
